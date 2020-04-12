9f70ba9ee53e631994095f4f07874cc0c9e499c4
Brookelynn Constant <Brookelynn.constant@gmail.com>
2020-04-12 13:53:40 -0500

    completed row css assignment for space debris

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,11 +20,12 @@ export class AppComponent {
        response.json().then(function(data) {
  
           let fetchedSatellites = data.satellites;
+          
           for (let i = 0; i < fetchedSatellites.length; i ++) {
-            let satellite = [];
-            new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-            this.sourceList.push(satellite);
+            let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            this.sourceList.push(satellite)
           }
+          
           // TODO: loop over satellites
           // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,7 +4,7 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string; 
-
+    
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
         this.name = name;
         this.type = type;
@@ -12,7 +12,16 @@ export class Satellite {
         this.orbitType = orbitType;
         this.operational = operational;
     }  
-  
+
+    
+
+    shouldShowWarning() :boolean {
+       if(this.type === 'Space Debris') {
+        return true
+        } else {return false
+        }
+    }   
+
   
 }
 
